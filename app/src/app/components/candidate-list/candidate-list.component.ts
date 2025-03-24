4f089413c5d288c1edf27a7d348bcb3294946693
Andrea Di Toro <a.ditoro@inpr3mium.com>
2025-03-25 00:27:53 +0100

    add  table pagination

--- app/src/app/components/candidate-list/candidate-list.component.ts
+++ app/src/app/components/candidate-list/candidate-list.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Candidate } from '../../../candidate';
 import { CommonModule } from '@angular/common';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'candidate-list',
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, CommonModule, MatPaginatorModule],
   templateUrl: './candidate-list.component.html',
 })
 
@@ -14,11 +15,16 @@ export class CandidateListComponent {
   @Input() candidates!: Candidate[];
 
   displayedColumns: string[] = ['name', 'surname', 'seniority', 'years', 'availability'];
-  dataSource = new MatTableDataSource(this.candidates);
+  dataSource = new MatTableDataSource<Candidate>(this.candidates);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    console.log(this.paginator)
+  }
   ngOnChanges(changes: SimpleChanges) {
     if (changes['candidates']) {
-      this.dataSource = new MatTableDataSource(this.candidates);
+      this.dataSource.data = this.candidates;
     }
   }
 
