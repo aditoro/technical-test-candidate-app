3a45e015100a38a1b6150a3307fbde7c9be7050c
Andrea Di Toro <a.ditoro@inpr3mium.com>
2025-03-24 18:50:48 +0100

    remove defaults specs

--- api/src/candidate/candidate.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CandidateEntity } from './candidate.entity';
-
-describe('CandidateEntity', () => {
-  it('should be defined', () => {
-    expect(new CandidateEntity()).toBeDefined();
-  });
-});
